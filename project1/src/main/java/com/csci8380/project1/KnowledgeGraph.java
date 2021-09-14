edf1d431fca358d143e5ab75f0edd2c604943e81
Zachary Ross <zachross015@gmail.com>
2021-09-14 15:07:46 -0400

    Updated the query

--- project1/src/main/java/com/csci8380/project1/KnowledgeGraph.java
+++ project1/src/main/java/com/csci8380/project1/KnowledgeGraph.java
@@ -35,48 +35,42 @@ public class KnowledgeGraph {
         QueryExecution queryExecution = QueryExecutionFactory.create(query, KnowledgeGraph.model);
         ResultSet resultSet = queryExecution.execSelect();
         List<String> solu_list = new ArrayList<String>();
-        while (resultSet.hasNext())
-            solu_list.add(resultSet.next().toString());
+        while (resultSet.hasNext()) {
+        	String next = resultSet.next().toString();
+            solu_list.add(next);
+        }
         return solu_list;
     }
 
     public static List<Paper> findCOI(String author_1, String author_2) {
    
         /* Initialize some variables */
-        List<String> solutions_1 = new ArrayList<String>();
-        List<String> solutions_2 = new ArrayList<String>();
         List<String> solutions = new ArrayList<String>(); 
         List<Paper> papers = new ArrayList<Paper>();
 
         /* SPARQL query sentences */
         Query query_1 = QueryFactory.create(
-                "SELECT ?o1 ?o2 WHERE {?x <http://purl.org/dc/elements/1.1/title> ?o1 . " +
-                        "?x <http://purl.org/dc/terms/issued> ?o2 . " +
-                        "{SELECT ?x WHERE {?x <http://xmlns.com/foaf/0.1/maker> ?y . " +
-                        "{SELECT ?y WHERE {?y <http://xmlns.com/foaf/0.1/name> \"" +
-                        author_1 +
-                        "\" .}}}}}",
-                Syntax.syntaxSPARQL_11
-        );
-        Query query_2 = QueryFactory.create(
-                "SELECT ?o1 ?o2 WHERE {?x <http://purl.org/dc/elements/1.1/title> ?o1 . " +
-                        "?x <http://purl.org/dc/terms/issued> ?o2 . " +
-                        "{SELECT ?x WHERE {?x <http://xmlns.com/foaf/0.1/maker> ?y . " +
-                        "{SELECT ?y WHERE {?y <http://xmlns.com/foaf/0.1/name> \"" +
-                        author_2 +
-                        "\" .}}}}}",
+                "SELECT ?o1 ?o2 WHERE {"
+                + "  ?x <http://purl.org/dc/elements/1.1/title> ?o1 ."
+                + "  ?x <http://purl.org/dc/terms/issued> ?o2 ."
+                + "  {"
+                + "	SELECT DISTINCT ?x WHERE {"
+                + "	  ?x <http://xmlns.com/foaf/0.1/maker> ?y ."
+                + "	  ?x <http://xmlns.com/foaf/0.1/maker> ?z ."
+                + "      {"
+                + "		SELECT ?y ?z WHERE {"
+                + "		  ?y <http://xmlns.com/foaf/0.1/name> \"" + author_1  + "\" ."
+                + "		  ?z <http://xmlns.com/foaf/0.1/name> \"" + author_2  + "\" ."
+                + "		}"
+                + "	  }"
+                + "	}"
+                + "  }"
+                + "}",
                 Syntax.syntaxSPARQL_11
         );
 
         /* Do query */
-        solutions_1 = queryGraph(query_1);
-        solutions_2 = queryGraph(query_2);
-
-        /* Find intersection */
-        for (String s: solutions_1)
-            if (solutions_2.contains(s))
-                solutions.add(s);
-
+        solutions = queryGraph(query_1);
 
         /* Format output lists */
         for (String element: solutions) {
