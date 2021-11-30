2bcd46317e855f2b299b24360a2d83c5b29330ae
Zhiliang Lyu <90400056+zllyu@users.noreply.github.com>
2021-11-30 15:14:25 -0500

    Update data_model.py

--- final_project/pdb_kg/downloader/data_model.py
+++ final_project/pdb_kg/downloader/data_model.py
@@ -36,6 +36,22 @@ class Label(enum.IntEnum):
     """
     Indicates that a node represents an ontology annotation.
     """
+    DRUGBANKTARGET = enum.auto()
+    """
+    Indicates that a node represents a drug's target.
+    """
+    DATABASE = enum.auto()
+    """
+    Indicates that a node represents a protein database.
+    """
+    RCSBENTITYHOSTORGANISM = enum.auto()
+    """
+    Indicates that a node represents a host organism of a protein.
+    """
+    RCSBENTITYSOURCEORGANISM = enum.auto()
+    """
+    Indicates that a node represents a source organism of a protein.
+    """
 
 
 class NodeBase(BaseModel):
@@ -107,8 +123,10 @@ class ProteinNode(Entity):
 
     Attributes:
         entry_id: The ID of the parent PDB entry for this protein.
+        name: The name of the parent PDB entry for this protein.
 
         sequence: The sequence of the protein, in FASTA notation.
+        sequence: The length of the protein sequence.
 
         annotations: Gene ontology annotations that are associated with this
             protein. They should be labeled by the GO ID.
@@ -119,11 +137,57 @@ class ProteinNode(Entity):
     label: Label = Label.PROTEIN
 
     entry_id: str
+    name: str
 
     sequence: str
+    sequence_length: int
 
     annotations: Set[str]
     cofactors: Set[str]
+
+
+class RcsbEntityHostOrganism(NodeBase):
+    """
+    A node representing a host organism of a protein.
+
+    Attributes:
+        common_names: Common names from NCBI.
+        parent_scientific_name: Parent's scientific name from NCBI.
+        scientific_name: Scientific name from NCBI.
+        taxonomy_id: Taxonomy ID from NCBI.
+        provenance_source: What kind of data is the organism.
+    """
+
+    label: Label = Label.RCSBENTITYHOSTORGANISM
+
+    common_names: Set[str]
+    parent_scientific_name: str
+    scientific_name: str
+    taxonomy_id: int
+    provenance_source: str
+
+
+class RcsbEntitySourceOrganism(NodeBase):
+    """
+    A node representing a source organism of a protein.
+
+    Attributes:
+        common_names: Common names from NCBI.
+        parent_scientific_name: Parent's scientific name from NCBI.
+        scientific_name: Scientific name from NCBI.
+        taxonomy_id: Taxonomy ID from NCBI.
+        provenance_source: What kind of data is the organism.
+        source_type: Represents the type of the source organism.
+    """
+
+    label: Label = Label.RCSBENTITYSOURCEORGANISM
+
+    common_names: Set[str]
+    parent_scientific_name: str
+    scientific_name: str
+    taxonomy_id: int
+    provenance_source: str
+    source_type: str
 
 
 class DrugNode(Entity):
@@ -132,6 +196,8 @@ class DrugNode(Entity):
 
     Attributes:
         drug_groups: Represents the drug groups that this entity belongs to.
+        drugbank_id: Identity of the node in DrugBank.
+        name: The compound official name.
         synonyms: Synonymous names for the drug.
 
     """
@@ -139,7 +205,45 @@ class DrugNode(Entity):
     label: Label = Label.DRUG
 
     drug_groups: Set[str]
+    drugbank_id: str
+    name: str
     synonyms: Set[str]
+
+
+class DrugbankTarget(NodeBase):
+    """
+    Described the specific drug's target and where can find the target.
+
+    Attributes:
+        interaction_type: Represents interaction types.
+        name: Reprsnts the target's name.
+        ordinal: Distinguish different targets on the same drug.
+        organism_common_name: Represents the target affect whom.
+        seq_one_letter_code: Represents an amino acid sequence of the target.
+    """
+
+    label: Label = Label.DRUGBANKTARGET
+
+    interaction_type: str
+    name: str
+    ordinal: int
+    organism_common_name: str
+    seq_one_letter_code: str
+
+
+class Database(NodeBase):
+    """
+    A node representing which database can find the drug/protein's components.
+
+    Attributes:
+        reference_database_accession_code: Identity of the database where the protein or drug/target comes from.
+        reference_database_name: Represents where the protein or drug's target comes from.
+    """
+
+    label: Label = Label.DATABASE
+
+    reference_database_accession: str
+    reference_database_name: str
 
 
 class AnnotationNode(Entity):
